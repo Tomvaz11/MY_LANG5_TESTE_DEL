2bbc2b30cdaf4ad25cb0c75770dfd6d3bcb0cb2e
Tomvaz11 <vr.antonio01@gmail.com>
2025-03-06 15:07:57 -0300

    Delete my_lang directory

--- my_lang/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Chatbot com LangMem - Um chatbot com memória de longo prazo.
-"""
-
-__version__ = "1.0.0" 
--- my_lang/src/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Módulo da API para o chatbot com LangMem.
-"""
-
-from src.api.routes import create_api
-
-__all__ = ["create_api"] 
